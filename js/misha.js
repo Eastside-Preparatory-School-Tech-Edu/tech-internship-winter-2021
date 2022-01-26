4ba73420d4c4a1a05f3cc0b0b6ca8bbf139d9c59
mnivota <misha.nivota@outlook.com>
2022-01-26 00:22:34 -0800

    adding custom activities and changing break times

--- js/misha.js
+++ js/misha.js
@@ -3,7 +3,34 @@ function SwitchAlertMode() {
 
 }
 
+//RELATING TOO 3 FUNCTIONS BEFORE
 // Function to change time for breaks
-function AdjustTimer() {
+function AdjustTimer1() {
+    var time = document.getElementById("breakTime1").value;
 
+    //temp action in place of changing time value
+    document.getElementById("newTime").innerHTML = "Your new break time is: " + time;
+}
+
+function AdjustTimer2() {
+    var time = document.getElementById("breakTime2").value;
+
+    //temp action in place of changing time value
+    document.getElementById("newTime").innerHTML = "Your new break time is: " + time;
+}
+
+function AdjustTimer3() {
+    var time = document.getElementById("breakTime3").value;
+
+    //temp action in place of changing time value
+    document.getElementById("newTime").innerHTML = "Your new break time is: " + time;
+}
+
+
+//Function to add custom activity to list
+function addActivity() {
+    var act = document.getElementById("customAct").value;
+
+    //temp action in place of adding custom activity to activity list
+    document.getElementById("inputAct").innerHTML = "Your custom activity is: " + act;
 }
