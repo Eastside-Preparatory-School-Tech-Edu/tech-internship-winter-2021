61fa14b4c7250ec60c4c5585ddf6fb21bdca0900
dgoetz987 <87041840+dgoetz987@users.noreply.github.com>
2022-01-25 22:45:37 -0800

    Added Modal
    Added Modal and other html stuff that we actual need. Did not finish audio, will give full explanation tomorrow

--- js/drew.js
+++ js/drew.js
@@ -1,9 +1,10 @@
-// main clock that checks events
-let [mainHours, mainMinutes, mainSeconds] = [0, 0, 0];
-// array of events and their data
-let timerEvents = []
+/* declare main variables */
+let mainHours, mainMinutes, mainSeconds;
+let timerEvents = [];
+var popupModal;
+var closeButton;
 
-// ___________ Optional OOP stuff I would like to have implemented throughout ___________
+/* ___________ Optional OOP stuff I would like to have implemented throughout ___________ */
 
 // event class to hold events and their relevant metadata
 class timerEvent {
@@ -15,29 +16,42 @@ class timerEvent {
     }
 }
 
-// class to carry times easily for timerEvent class
+/* class to carry times easily for timerEvent class */
 class time {
     constructor(hours, minutes, seconds) {
         this.hours = hours;
         this.minutes = minutes;
-        this.seconds = this.seconds;
+        this.seconds = seconds;
     }
 }
 
-// create a new event with given parameters
+/* create a new event with given parameters */
 function createNewEvent(name, description, startTime, endTime) {
     timerEvents.push(new timerEvent(name, description, startTime, endTime));
 }
 
-// _______________ Normal Stuff resumes below _____________________
+/* _______________ Normal Stuff resumes below _____________________ */
 
-// main function that orients the program
-function main() {
-    TimerUpdate();
-    CheckIfEventShouldFire(mainHours, mainMinutes, mainSeconds, timerEvents);
+/* initiate some variables BEFORE main gets going */
+function startup() {
+    console.log("working");
+    popupModal = document.getElementById("alertModal");
+    closeButton = document.getElementsByClassName("close")[0];
+    // close function for modal
+    closeButton.onclick = function() {
+        popupModal.style.display = "none";
+    }
 }
 
-// update the main body timer
+/* main function that orients the program */
+function main() {
+    /* update timer */
+    TimerUpdate();
+    //CheckIfEventShouldFire(mainHours, mainMinutes, mainSeconds, timerEvents);
+    crudeAlertFire();
+}
+
+/* update the main body timer */
 function TimerUpdate() {
     dt = new Date($.now());
     mainHours = dt.getHours();
@@ -45,19 +59,31 @@ function TimerUpdate() {
     mainSeconds = dt.getSeconds();
 }
 
-// compare event timings to the time
+/* compare event timings to the time */
 function CheckIfEventShouldFire(hours, minutes, seconds, events) {
     events.forEach(element => {
         if (element.hours);
     });
 }
 
-// Function to display the activity
-function Popup() {
-
+/* Temp crude version while we figure out when we want this to fire */
+function crudeAlertFire() {
+    /*
+    if (mainMinutes % 30 == 0 && seconds == 0) {
+        Popup();
+        AlertSound();
+    }*/
+    Popup();
+    AlertSound();
 }
 
-// Function to play the sound
+/* Function to display the activity */
+function Popup() {
+    document.getElementById("modalText").innerHTML = "This Text can Be changed";
+    popupModal.style.display = "block";
+}
+
+/* Function to play the sound */
 function AlertSound() {
 
 }
