f5b72d806eae9a173c589c489f8ac3a12049e6d1
mnivota <misha.nivota@outlook.com>
2022-01-31 14:36:45 -0800

    Changed time adjusting fuction

--- js/misha.js
+++ js/misha.js
@@ -5,25 +5,13 @@ function SwitchAlertMode() {
 
 //RELATING TOO 3 FUNCTIONS BEFORE
 // Function to change time for breaks
-function AdjustTimer1() {
-    var time = document.getElementById("breakTime1").value;
-
-    //temp action in place of changing time value
-    document.getElementById("newTime").innerHTML = "Your new break time is: " + time;
-}
-
-function AdjustTimer2() {
-    var time = document.getElementById("breakTime2").value;
-
-    //temp action in place of changing time value
-    document.getElementById("newTime").innerHTML = "Your new break time is: " + time;
-}
-
-function AdjustTimer3() {
-    var time = document.getElementById("breakTime3").value;
-
-    //temp action in place of changing time value
-    document.getElementById("newTime").innerHTML = "Your new break time is: " + time;
+function AdjustTimer() {
+    fullTime = document.getElementById("breakTime").value;
+    hours = fullTime[0] + fullTime[1]
+    minutes = fullTime[3] + fullTime[4]
+    document.getElementById("breakOutput").innerHTML = "Your break time is: " + fullTime;
+    document.getElementById("breakHours").innerHTML = "Hour: " + hours;
+    document.getElementById("breakMinutes").innerHTML = "Minutes: " + minutes;
 }
 
 
